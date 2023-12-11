a9784e4aa89fafefabcb95515bdd0b7fa90e3a16
job monari <jobmonari7@gmail.com>
2023-12-11 14:46:34 +0300

    adding postgresql and docker compose

--- iluvcoffee/src/app.module.ts
+++ iluvcoffee/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule],
 })
 export class AppModule {}
