6f56b406a0d38b1bd79e72c409447bc94718b7b6
job monari <jobmonari7@gmail.com>
2023-12-11 12:24:50 +0300

    acting on controllers

--- iluvcoffee/src/coffees/coffees.controller.ts
+++ iluvcoffee/src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -6,4 +6,21 @@ export class CoffeesController {
     findAll():string{
         return "These action returns all coffees";
     }
+
+    @Get(':id')
+    findOne(@Param('id') id:string):string{
+        return `this returns #${id} coffee`
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() body){
+        return body;
+    }
+
+    @Patch(':id')
+    update(id:string, body){
+        return `this action updates ${id} coffee`
+    }
+
 }
