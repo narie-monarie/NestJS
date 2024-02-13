88e6540240915492ed37965c4faab3e91f3fedf4
Job Monari <jobmonari7@gmail.com>
2024-02-13 18:47:18 +0300

    HTTP methods

--- iluvcoffee/src/app.module.ts
+++ iluvcoffee/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CoffeesController } from './coffees/coffees.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CoffeesController],
+  providers: [], 
 })
 export class AppModule {}
