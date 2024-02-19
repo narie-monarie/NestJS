c4378c2e211c6a0a9f2a8fb8e37bb5fd927730c0
Job Monari <jobmonari7@gmail.com>
2024-02-19 12:08:41 +0300

    TypeORM entity

--- iluvcoffee/src/app.module.ts
+++ iluvcoffee/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'monari',
+      password: 'secret',
+      database: 'narie',
+      autoLoadEntities: true,
+      //disable in production
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
