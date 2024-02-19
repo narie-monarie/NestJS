9a56988281605370f34f2499a981b59f88c160d0
Job Monari <jobmonari7@gmail.com>
2024-02-19 09:27:00 +0300

    removee

--- iluvcoffee/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoffeesModule } from './coffees/coffees.module';
-
-@Module({
-  imports: [CoffeesModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
